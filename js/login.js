f7a0794a7381bab47fa409ec9520908a699ad034
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-10-12 01:11:17 +0600

    log in redirect to home page

--- js/login.js
+++ js/login.js
@@ -1,10 +1,24 @@
 document.getElementById("btn-login").addEventListener("click", function(event) {
     event.preventDefault();
-    console.log("ami click");
+
 
     let mobileNumber = document.getElementById("mobile-number");
     let mobile = mobileNumber.value;
-    console.log(mobile);
+    // console.log(mobile);
+
+    let pinNumber = document.getElementById("pin-Number");
+    let pin = pinNumber.value;
+    // console.log(pin);
+
+    if (mobile === "0153" && pin === "1234") {
+
+        window.location.href = "/home.html";
+
+    } else {
+
+        alert("Please enter  valid mobile or pin ");
+
+    }
 
 
 })
